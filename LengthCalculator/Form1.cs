a900a899858c14b200d79bdc1e8d88c66a08b80d
isa0421 <94669156+isa0421@users.noreply.github.com>
2024-04-05 19:24:21 +0800

    Initial commit

---
+++ LengthCalculator/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LengthCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCM_KeyUp(object sender, KeyEventArgs e)
+        {
+            double douCM; 
+
+            douCM = Convert.ToDouble(txtCM.Text); 
+
+            txtM.Text = string.Format("{0:0.##########}", douCM / 100);
+        }
+    }
+}
